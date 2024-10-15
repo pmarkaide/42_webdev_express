bc89562bf392033facab1e98dcf13cd06f79bf55
Rich <applerich0306@gmail.com>
2024-10-15 14:15:54 +0300

    removed cache

--- frontend/src/pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-// src/pages/_app.tsx
-import { AppProps } from 'next/app';
-import './global.css'; // Ensure to import your global CSS file
-
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
