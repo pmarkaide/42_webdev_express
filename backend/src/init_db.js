6c02cf9ea441f875bbc4ef3da2ae2334c927dfc2
Rich <applerich0306@gmail.com>
2024-10-16 18:58:30 +0300

    rich testing postgres

---
+++ backend/src/init_db.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const path = require('path');
+const pool = require('./db');
+
+const runSQLScript = async () => {
+  try {
+    const sqlPath = path.join(__dirname, 'init_db.sql');
+    const sql = fs.readFileSync(sqlPath).toString();
+
+    await pool.query(sql);
+    console.log('SQL script executed successfully.');
+  } catch (err) {
+    console.error('Error executing SQL script:', err);
+  } finally {
+    await pool.end();
+  }
+};
+
+// Run the script
+runSQLScript();
