06e3fb244842be6556324b3c0172c3ca6c610100
Pablo Markaide <pablo.markaide@gmail.com>
2024-10-17 12:22:48 +0300

    fix: fix the search again

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
-import Header from '../components/Header';
 import Footer from '../components/Footer';
 import Main from '../components/Main';
 
 const Home: React.FC = () => {
   return (
     <div>
-      <Header />
-      <Main />
+      <Main searchQuery="" />
       <Footer />
     </div>
   );
