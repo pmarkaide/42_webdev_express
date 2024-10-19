925a3761e82b3729586ba3722077a0821aa9db63
Rich <applerich0306@gmail.com>
2024-10-19 15:51:59 +0300

    try to commit

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -54,7 +54,8 @@ const Header: React.FC = () => {
             </a>
             <a className="inline-block rounded-lg px-2 py-1 text-sm font-medium text-gray-900 transition-all duration-200 hover:bg-gray-100 hover:text-gray-900" href="#">
               shopping center
-            </a>
+			</a>
+			<p>searach bar Palbo</p>
           </div>
           <div className="flex items-center justify-end gap-3 relative">
             {token ? (
